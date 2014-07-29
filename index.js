7489129e4078ab5881222af457c4039b28cd620f
Matt McKegg <matt@wetsand.co.nz>
2014-07-29 13:31:07 +1200

    extracted audio-context.js to loop-drop-audio-context

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var behave = require('./behaviors')
 
 window.events = new EventEmitter()
 window.context = {
-  audio: require('./audio-context')
+  audio: require('loop-drop-audio-context')
 }
 
 // apply behaviors
