3ad705cfb55a7c8ecd222002c4b82abf44866198
Matt McKegg <matt@wetsand.co.nz>
2014-11-01 01:59:43 +1300

    add qwerty keyboard controller support
    closes #7

--- midi-controllers.js
+++ midi-controllers.js
@@ -1,10 +1,12 @@
 module.exports = require('loop-grid')
+module.exports.qwerty = require('loop-qwerty')
 module.exports.launchpad = require('loop-launchpad')
 module.exports.mpkmini = require('loop-mpkmini')
 module.exports.apcmini = require('loop-apcmini')
 
 module.exports._choices = [
   ['No Controller', 'controller'],
+  ['Qwerty Keyboard', 'controller/qwerty'], 
   ['Novation Launchpad', 'controller/launchpad'],
   ['Akai APC Mini', 'controller/apcmini'],
   ['Akai MPK Mini', 'controller/mpkmini']
