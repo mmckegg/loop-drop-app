89126af07d3c96917cc4a0b0bfac5198b795eb29
Matt McKegg <matt@wetsand.co.nz>
2015-02-21 01:47:25 +1300

    drag and drop reorder of slot triggers

--- views/editor/nodes/chunk/slot-chooser.js
+++ views/editor/nodes/chunk/slot-chooser.js
@@ -1,5 +1,7 @@
 var mercury = require('mercury')
 var h = require('micro-css/h')(mercury.h)
+var MPE = require('../../../../lib/mouse-position-event.js')
+var nextTick = require('next-tick')
 
 module.exports = renderOptions
 
@@ -26,10 +28,19 @@ function renderOptions(chunk){
       var id = String(i)
       var slot = slots.get(id)
       var width = (100 / shape[1]) + '%'
+      var dragInfo = { collection: chunk.slots, id: id, select: chunk.selectedSlotId.set }
 
       if (slot){
         triggers.push(
           h('div.slot', {
+            'draggable': true,
+
+            'ev-dragstart': MPE(dragStart, dragInfo),
+            'ev-dragend': MPE(dragEnd, dragInfo),
+            'ev-dragover': MPE(dragOver, dragInfo),
+            'ev-dragleave': MPE(dragLeave, dragInfo),
+            'ev-drop': MPE(drop, dragInfo),
+
             'style': {width: width},
             'className': selectedSlotId === id ? '-selected' : '',
             'ev-click': mercury.event(chunk.selectedSlotId.set, id)
@@ -44,6 +55,11 @@ function renderOptions(chunk){
         triggers.push(
           h('div.slot -spawn', {
             'style': {width: width},
+
+            'ev-dragover': MPE(dragOver, dragInfo),
+            'ev-dragleave': MPE(dragLeave, dragInfo),
+            'ev-drop': MPE(drop, dragInfo),
+
             'ev-click': mercury.event(spawnSlot, { id: id, chunk: chunk })
           }, '+ trigger')
         )
@@ -81,4 +97,74 @@ function spawnSlot(ev){
   })
 
   chunk.selectedSlotId.set(id)
+}
+
+var currentDrag = null
+
+function dragStart(ev){
+  currentDrag = ev.data
+}
+
+function dragEnd(ev){
+  currentDrag = null
+}
+
+function dragOver(ev){
+  ev.currentTarget.classList.add('-dragOver')
+  if (ev.altKey){
+    ev.dataTransfer.dropEffect = 'copy'
+  } else {
+    ev.dataTransfer.dropEffect = 'move'
+  }
+  ev.event.preventDefault()
+}
+
+function drop(ev){
+  var sourceCollection = currentDrag.collection
+  var targetCollection = ev.data.collection
+
+  var sourceLookup = sourceCollection.context.slotLookup
+  var targetLookup = targetCollection.context.slotLookup
+
+  var source = targetLookup.get(currentDrag.id)
+  var target = sourceLookup.get(ev.data.id)
+
+  var isCopying = ev.altKey
+
+  if (source && source !== target){
+
+    if (target){
+      // clear out existing
+      targetCollection.remove(target)
+    }
+
+    if (sourceCollection !== targetCollection || isCopying){
+
+      // move to different collection
+      var descriptor = obtainWithId(ev.data.id, source())
+
+      if (!isCopying){
+        sourceCollection.remove(source)
+      }
+
+      targetCollection.push(descriptor)
+    
+    } else {
+      source.id.set(ev.data.id)
+    }
+
+    ev.data.select(ev.data.id)
+  }
+
+  dragLeave(ev)
+}
+
+function dragLeave(ev){
+  ev.currentTarget.classList.remove('-dragOver')
+}
+
+function obtainWithId(id, obj){
+  obj = JSON.parse(JSON.stringify(obj))
+  obj.id = id
+  return obj
 }
