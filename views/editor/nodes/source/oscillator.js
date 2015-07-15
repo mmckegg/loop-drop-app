e62db38a94079169f5977f02b7776fb51abff3f4
Matt McKegg <matt@wetsand.co.nz>
2015-07-15 17:29:27 +1200

    oscillator: add a bunch of periodic waves for spice

--- views/editor/nodes/source/oscillator.js
+++ views/editor/nodes/source/oscillator.js
@@ -7,10 +7,34 @@ var ModRange = require('lib/params/mod-range')
 var Select = require('lib/params/select')
 
 var shapeChoices = [
-  ['Sine', 'sine'],
-  ['Square', 'square'],
-  ['Sawtooth', 'sawtooth'],
-  ['Triangle', 'triangle']
+  ['Basic Shapes', [
+    ['Sine', 'sine'],
+    ['Square', 'square'],
+    ['Sawtooth', 'sawtooth'],
+    ['Triangle', 'triangle']
+  ]],
+  ['Wave Tables', [
+    ['Bass', 'bass'],
+    ['Bass Fuzz', 'bass-fuzz'],
+    ['Bass Sub', 'bass-sub'],
+    ['Brass', 'brass'],
+    ['Disonant', 'disonant'],
+    ['Guitar Fuzz', 'guitar-fuzz'],
+    ['Organ', 'organ'],
+    ['Piano', 'piano'],
+    ['Soft Saw', 'soft-saw'],
+    ['Strings', 'strings'],
+    ['Throaty', 'throaty'],
+    ['Trombone', 'trombone'],
+    ['Wide', 'wide'],
+    ['Wurlitzer', 'wurlitzer']
+  ]],
+  ['Phonemes', [
+    ['Ah', 'ah'],
+    ['Ee', 'ee'],
+    ['Ow', 'ow'],
+    ['Ooh', 'ooh']
+  ]]
 ]
 
 module.exports = function(node){
