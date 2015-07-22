dc502c5b18ea64e7d49f47e5061b7baf5cad62bd
Matt McKegg <matt@wetsand.co.nz>
2015-07-22 18:41:32 +1200

    granular: add beat synced editor

--- views/editor/nodes/source/granular.js
+++ views/editor/nodes/source/granular.js
@@ -34,24 +34,6 @@ module.exports = function(node){
         defaultValue: 'loop'
       }),
 
-      ToggleButton(node.sync, {
-        title: 'BPM Sync'
-      }),
-
-      Range(node.duration, {
-        title: 'duration',
-        defaultValue: 1,
-        format: isSyncing ? 'beats' : 'ms',
-        flex: true
-      }),
-
-      Range(node.rate, {
-        title: 'rate',
-        defaultValue: 16,
-        format: 'ratio32',
-        flex: true
-      }),
-
       ModRange(node.amp, {
         title: 'amp',
         format: 'dB',
@@ -65,9 +47,69 @@ module.exports = function(node){
         flex: true
       }),
 
+    ]),
+
+    h('h1', 'Timing'),
+
+    h('ParamList', [
+
+      ToggleButton(node.sync, {
+        title: 'BPM Sync'
+      }),
+
+      node.sync() ? [
+
+        Range(node.sync.tempo, {
+          title: 'bpm',
+          format: 'bpm',
+          flex: true
+        }),
+
+        Range(node.sync.trim, {
+          title: 'start',
+          format: 'ratio1',
+          width: 100,
+          flex: true
+        }),
+
+        Range(node.sync.beatOffset, {
+          title: 'beat offset',
+          format: 'beatOffset',
+          width: 100,
+          flex: true
+        }),
+
+        Range(node.sync.beats, {
+          title: 'beats',
+          format: 'beats2',
+          flex: true
+        })
+
+      ] : [
+        Range(node.duration, {
+          title: 'duration',
+          defaultValue: 1,
+          format: 'ms',
+          flex: true
+        })
+      ]
+    ]),
+
+    h('h1', 'Grains'),
+
+    h('ParamList', [
+
+      Range(node.rate, {
+        title: 'rate',
+        defaultValue: 16,
+        format: 'ratio32',
+        flex: true
+      }),
+
       Range(node.attack, {
         title: 'attack',
         format: 'ratio',
+        width: 100,
         defaultValue: 0.1,
         flex: true
       }),
@@ -76,6 +118,7 @@ module.exports = function(node){
         title: 'hold',
         format: 'ratio',
         defaultValue: 1,
+        width: 100,
         flex: true
       }),
 
@@ -83,6 +126,7 @@ module.exports = function(node){
         title: 'release',
         format: 'ratio',
         defaultValue: 0.1,
+        width: 100,
         flex: true
       })
 
