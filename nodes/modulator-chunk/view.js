3e00749f24194fff744a5af9d730d2ae883486b5
Matt McKegg <matt@wetsand.co.nz>
2015-09-02 02:46:08 +1200

    modulator-chunk/view: use lib/widgets/chunk

--- nodes/modulator-chunk/view.js
+++ nodes/modulator-chunk/view.js
@@ -8,25 +8,10 @@ var RenameHook = require('lib/rename-hook')
 
 var IndexParam = require('lib/index-param')
 var FlagParam = require('lib/flag-param')
+var renderChunk = require('lib/widgets/chunk')
 
 module.exports = function renderModulatorChunk (node) {
-  var setup = node.context.setup
-  var actions = node.context.actions
-  var collection = node.context.collection
   var slotLookup = node.context.slotLookup
-
-  var selected = setup.selectedChunkId() == node.id()
-  var headerStyle = {
-    backgroundColor: color(node.color(), selected ? 0.5 : 0.1)
-  }
-  var mainStyle = {
-    border: '2px solid '+color(node.color(), selected ? 1 : 0)
-  }
-
-  var classNames = [] 
-  if (node.minimised()) classNames.push('-minimised')
-  if (selected) classNames.push('-selected')
-
   var elements = []
   var shape = node.shape()
   var length = shape[0] * shape[1]
@@ -49,42 +34,27 @@ module.exports = function renderModulatorChunk (node) {
     }
   }
 
-  return h('div ExternalNode', {
-    className: classNames.join(' '),
-    'ev-click': send(setup.selectedChunkId.set, node.id()),
-    'style': mainStyle
-  }, [
-    h('header', {
-      style: headerStyle
-    }, [
-      h('button.twirl', {
-        'ev-click': send(toggleParam, node.minimised)
-      }),
-      h('span', {'ev-hook': RenameHook(node, selected, actions.updateChunkReferences)}), 
-      h('span.type', ['modulator']),
-      h('button.remove Button -warn', {
-        'ev-click': send(collection.remove, node),
-      }, 'X')
-    ]),
-    node.minimised() ? '' : h('section', [
-      h('div ParamList', [
-        shapeParams(node.shape),
-        ToggleButton(FlagParam(node.flags, 'noRepeat'), {
-          title: 'Use Repeat', 
-          onValue: false,
-          offValue: true 
-        })
-      ]),
-
-      h('ValueSlots', elements)
-    ])
-  ])
+  return renderChunk(node, {
+    main: [
+      h('section', [
+        h('div ParamList', [
+          shapeParams(node.shape),
+          ToggleButton(FlagParam(node.flags, 'noRepeat'), {
+            title: 'Use Repeat',
+            onValue: false,
+            offValue: true
+          })
+        ]),
+        h('ValueSlots', elements)
+      ])
+    ]
+  })
 }
 
 function shapeParams(param){
   return [
     h('div -block -flexSmall', [
-      h('div', Range(IndexParam(param, 0), { 
+      h('div', Range(IndexParam(param, 0), {
         title: 'rows',
         format: 'bit',
         defaultValue: 1
@@ -92,7 +62,7 @@ function shapeParams(param){
     ]),
 
     h('div -block -flexSmall', [
-      h('div', Range(IndexParam(param, 1), { 
+      h('div', Range(IndexParam(param, 1), {
         title: 'cols',
         format: 'bit',
         defaultValue: 1
