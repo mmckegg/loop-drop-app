808fa32c549fd4dec0a91411fceb40c997da63f7
Matt McKegg <matt@wetsand.co.nz>
2015-10-05 16:17:30 +1300

    nodes: use bulk-require module

--- nodes/index.js
+++ nodes/index.js
@@ -1,33 +1,18 @@
-var fs = require('fs')
-var join = require('path').join
+var bulk = require('bulk-require')
+var nodes = bulk(__dirname, [ '*/index.js' ])
 
 var self = module.exports = []
 self.lookup = {}
 self.objectLookup = {}
 self.groupLookup = {}
 
-fs.readdirSync(__dirname).forEach(function (entry) {
-  var path = join(__dirname, entry, 'index.js')
-  var item = tryRequire(path)
-  if (item) {
-    self.push(item)
-    self.lookup[item.node || entry] = item
-    self.objectLookup[item.node || entry] = item.object
-    if (item.group) {
-      self.groupLookup[item.group] = self.groupLookup[item.group] || []
-      self.groupLookup[item.group].push(item)
-    } 
+Object.keys(nodes).forEach(function (key) {
+  var item = nodes[key].index
+  self.push(item)
+  self.lookup[item.node || entry] = item
+  self.objectLookup[item.node || entry] = item.object
+  if (item.group) {
+    self.groupLookup[item.group] = self.groupLookup[item.group] || []
+    self.groupLookup[item.group].push(item)
   }
 })
-
-function tryRequire (path) {
-  try {
-    return require(path)
-  } catch (ex) {
-    if (ex.code === 'MODULE_NOT_FOUND') {
-      return null
-    } else {
-      throw ex
-    }
-  }
-}
