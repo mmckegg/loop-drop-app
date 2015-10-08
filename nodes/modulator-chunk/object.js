274522dc447991e00cdc6f32293c6566f7a3da2b
Matt McKegg <matt@wetsand.co.nz>
2015-10-08 20:56:12 +1300

    modulator-chunk: inherit from base-chunk

--- nodes/modulator-chunk/object.js
+++ nodes/modulator-chunk/object.js
@@ -1,5 +1,3 @@
-var ObservStruct = require('observ-struct')
-var Observ = require('observ')
 var Property = require('observ-default')
 var Event = require('geval')
 var NodeArray = require('observ-node-array')
@@ -7,20 +5,19 @@ var ArrayGrid = require('array-grid')
 var computed = require('observ/computed')
 var lookup = require('observ-node-array/lookup')
 var Transform = require('audio-slot-param/transform')
+var BaseChunk = require('lib/base-chunk')
 
 module.exports = ModulatorChunk
 
-function ModulatorChunk(parentContext){
-
+function ModulatorChunk (parentContext) {
   var context = Object.create(parentContext)
+  var slots = NodeArray(context)
+  context.slotLookup = lookup(slots, 'id')
 
-  var obs = ObservStruct({
-    id: Observ(),
-    slots: NodeArray(context),
+  var obs = BaseChunk(context, {
+    slots: slots,
     minimised: Property(false),
-    shape: Property([1,4]),
-    color: Property([0,0,0]),
-    flags: Property([])
+    color: Property([0,0,0])
   })
 
   obs._type = 'ModulatorChunk'
@@ -28,83 +25,44 @@ function ModulatorChunk(parentContext){
   obs.context = context
   context.chunk = obs
 
-  context.slotLookup = lookup(obs.slots, 'id')
 
   var broadcastSchedule = null
-  obs.onSchedule = Event(function(b){
+  obs.onSchedule = Event(function (b) {
     broadcastSchedule = b
   })
 
   var currentTransform = null
 
-  obs.slots.onUpdate(function(){
-
-    if (currentTransform){
+  obs.slots.onUpdate(function () {
+    if (currentTransform) {
       currentTransform.destroy()
       currentTransform = null
     }
 
     var transforms = [0]
 
-    obs.slots.forEach(function(slot){
-      if (slot.onSchedule){
+    obs.slots.forEach(function (slot) {
+      if (slot.onSchedule) {
         transforms.push({param: slot, transform: operation})
       }
     })
 
     currentTransform = Transform(context, transforms)
     currentTransform.onSchedule(broadcastSchedule)
-
   })
 
-  obs.triggers = computed([obs.id, obs.shape], function(id, shape){
-    var length = shape[0] * shape[1]
-    var result = []
-    for (var i=0;i<length;i++){
-      result.push(String(i))
-    }
-    return result
-  })
-
-  obs.grid = computed([obs.triggers, obs.shape], ArrayGrid)
-
-  obs.triggerOn = function(id, at){
-    var slot = context.slotLookup.get(id)
-    if (slot){
-      slot.triggerOn(at)
-    }
-  }
-
-  obs.triggerOff = function(id, at){
-    var slot = context.slotLookup.get(id)
-    if (slot){
-      slot.triggerOff(at)
-    }
-  }
-
-  obs.destroy = function(){
-    if (currentTransform){
+  obs.destroy = function () {
+    if (currentTransform) {
       currentTransform.destroy()
       currentTransform = null
     }
   }
 
-  obs.resolvedGrid = computed([obs.triggers, obs.shape], function(triggers, shape){
-    return ArrayGrid(triggers.map(getGlobalId), shape)
-  })
-
   return obs
 
   // scoped
 
-  function operation(base, value){
-    return (parseFloat(base)||0) + (parseFloat(value)||0)
+  function operation (base, value) {
+    return (parseFloat(base) || 0) + (parseFloat(value) || 0)
   }
-
-  function getGlobalId(id){
-    if (id){
-      return obs.id() + '/' + id
-    }
-  }
-
 }
