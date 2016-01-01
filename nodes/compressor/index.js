e20eaae736a02ded3e2946cc9e28b2a59efdda7b
Matt McKegg <matt@wetsand.co.nz>
2016-01-02 01:12:07 +1300

    add compressor processor

---
+++ nodes/compressor/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Compressor',
+  node: 'processor/compressor',
+  group: 'processors',
+  object: require('audio-slot/processors/compressor'),
+  render: require('./view')
+}
---
+++ nodes/compressor/view.js
@@ -0,0 +1,41 @@
+var h = require('lib/h')
+var Header = require('lib/widgets/header')
+var ModRange = require('lib/params/mod-range')
+
+module.exports = function renderCompressor (node){
+  return h('ProcessorNode -compressor', [
+    Header(node, h('span', 'Compressor')),
+    h('ParamList', [
+      ModRange(node.threshold, {
+        title: 'threshold',
+        format: 'dBn',
+        flex: true
+      }),
+      ModRange(node.knee, {
+        title: 'knee',
+        format: 'dBn',
+        flex: true
+      }),
+      ModRange(node.ratio, {
+        title: 'ratio',
+        format: 'compressionRatio',
+        flex: true
+      }),
+      ModRange(node.reduction, {
+        title: 'reduction',
+        format: 'dBn',
+        flex: true
+      }),
+      ModRange(node.attack, {
+        title: 'attack',
+        format: 'ms',
+        flex: true
+      }),
+      ModRange(node.release, {
+        title: 'release',
+        format: 'ms',
+        flex: true
+      })
+    ])
+  ])
+}
