eba23fd6f4f978164c161ccb8cd0827a78f96e78
Matt McKegg <matt@wetsand.co.nz>
2016-06-19 12:03:12 -0700

    sample: default envelope and mode (oneshot)

--- nodes/sample/index.js
+++ nodes/sample/index.js
@@ -3,5 +3,13 @@ module.exports = {
   node: 'source/sample',
   group: 'sources',
   object: require('audio-slot/sources/sample'),
+  spawn: {
+    amp: {
+      node: 'modulator/adsr',
+      value: 1,
+      release: 0.01
+    },
+    mode: 'oneshot'
+  },
   render: require('./view')
 }
