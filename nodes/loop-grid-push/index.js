8b8ac498d535a13e90d74b3e7b04ae416904f5fd
Matt McKegg <matt@wetsand.co.nz>
2016-07-07 10:56:10 +1200

    fix default midi port on Ableton Push
    #186

--- nodes/loop-grid-push/index.js
+++ nodes/loop-grid-push/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Ableton Push',
   group: 'loop-grids',
-  portMatch: /^Ableton Push/,
+  portMatch: /^Ableton Push User/,
   node: 'controller/push',
   render: require('../loop-grid/view'),
   object: require('./object')
