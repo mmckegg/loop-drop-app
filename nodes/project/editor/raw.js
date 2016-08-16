56fab474f26ac47b7114ca726c7221e313aa7156
Matt McKegg <matt@wetsand.co.nz>
2016-08-16 21:30:46 +1200

    raw editor: destroy instance on remove from dom, upgrade brace dep

--- nodes/project/editor/raw.js
+++ nodes/project/editor/raw.js
@@ -1,4 +1,4 @@
-
+var h = require('lib/h')
 var JSMN = require('lib/jsmn.js')
 var deepEqual = require('deep-equal')
 var watch = require('observ/watch')
@@ -12,90 +12,102 @@ var NO_TRANSACTION = {}
 module.exports = RawEditor
 
 function RawEditor (fileObject) {
-  var element = document.createElement('div')
-  element.className = 'RawEditor'
-
-  var el = document.createElement('div')
-
-  var textEditor = ace.edit(el)
-  var release = null
-
-  window.editors = window.editors || []
-  window.editors.push(textEditor)
-
-  textEditor.setTheme('ace/theme/ambiance')
-  textEditor.session.setMode('ace/mode/javascript')
-  textEditor.session.setUseWorker(false)
-  textEditor.session.setTabSize(2)
-  textEditor.renderer.setScrollMargin(20, 100)
-  textEditor.renderer.setPadding(20)
-  textEditor.renderer.setShowGutter(false)
-
-  var currentFile = null
-
-  var currentTransaction = NO_TRANSACTION
-  var lastSave = NO_TRANSACTION
-
-  textEditor.setFile = function (fileObject) {
-    clearTimeout(saveTimer)
-
-    if (release) {
-      release()
-      release = null
-    }
-
-    currentFile = fileObject
-
-    if (fileObject) {
-      release = watch(fileObject, update)
-    }
-  }
-
-  function save () {
-    var value = textEditor.session.getValue()
-    if (currentFile) {
-      try {
-        var object = JSMN.parse(value)
-        lastSave = object
-        currentFile.set(object)
-      } catch (ex) {}
-    }
-  }
-
-  function update () {
-    var data = currentFile ? currentFile() : null
-    if (data && !deepEqual(lastSave, data)) {
-      var newValue = JSMN.stringify(data || {})
-      currentTransaction = newValue
-      textEditor.session.setValue(newValue, -1)
-      currentTransaction = NO_TRANSACTION
-    }
-  }
-
-  var blurTimer = null
-  textEditor.on('focus', function () {
-    clearTimeout(blurTimer)
+  return h('RawEditor', {
+    hooks: [
+      RawEditorHook(fileObject)
+    ]
   })
+}
 
-  textEditor.on('blur', function () {
-    clearTimeout(blurTimer)
-    blurTimer = setTimeout(function () {
-      if (!textEditor.isFocused()) {
-        update()
-      }
-    }, 100)
-  })
+function RawEditorHook (fileObject) {
+  return function (element) {
+    var el = document.createElement('div')
 
-  var saveTimer = null
-  textEditor.on('change', function () {
-    if (currentTransaction === NO_TRANSACTION) {
+    var textEditor = ace.edit(el)
+    var release = null
+
+    textEditor.setTheme('ace/theme/ambiance')
+    textEditor.session.setUseWorker(false)
+    textEditor.session.setMode('ace/mode/javascript')
+    textEditor.session.setTabSize(2)
+    textEditor.renderer.setScrollMargin(20, 100)
+    textEditor.renderer.setPadding(20)
+    textEditor.renderer.setShowGutter(false)
+    textEditor.$blockScrolling = Infinity
+
+    var currentFile = null
+
+    var currentTransaction = NO_TRANSACTION
+    var lastSave = NO_TRANSACTION
+
+    textEditor.setFile = function (fileObject) {
       clearTimeout(saveTimer)
-      saveTimer = setTimeout(save, 100)
+
+      if (release) {
+        release()
+        release = null
+      }
+
+      currentFile = fileObject
+
+      if (fileObject) {
+        release = watch(fileObject, update)
+      }
     }
-  })
 
-  textEditor.setFile(fileObject)
+    function save () {
+      var value = textEditor.session.getValue()
+      if (currentFile) {
+        try {
+          var object = JSMN.parse(value)
+          lastSave = object
+          currentFile.set(object)
+        } catch (ex) {}
+      }
+    }
 
-  element.appendChild(el)
-  return element
+    function update () {
+      var data = currentFile ? currentFile() : null
+      if (data && !deepEqual(lastSave, data)) {
+        var newValue = JSMN.stringify(data || {})
+        currentTransaction = newValue
+        textEditor.session.setValue(newValue, -1)
+        currentTransaction = NO_TRANSACTION
+      }
+    }
+
+    var blurTimer = null
+    textEditor.on('focus', function () {
+      clearTimeout(blurTimer)
+    })
+
+    textEditor.on('blur', function () {
+      clearTimeout(blurTimer)
+      blurTimer = setTimeout(function () {
+        if (!textEditor.isFocused()) {
+          update()
+        }
+      }, 100)
+    })
+
+    var saveTimer = null
+    textEditor.on('change', function () {
+      if (currentTransaction === NO_TRANSACTION) {
+        clearTimeout(saveTimer)
+        saveTimer = setTimeout(save, 100)
+      }
+    })
+
+    textEditor.setFile(fileObject)
+
+    element.appendChild(el)
+
+    return function () {
+      textEditor.destroy()
+      if (release) {
+        release()
+        release = null
+      }
+    }
+  }
 }
