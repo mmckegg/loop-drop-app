6f6fca3d198aba82a7900d1681dfc1f3da5b071e
Matt McKegg <matt@wetsand.co.nz>
2016-08-31 19:59:30 +1200

    slot: add choke group picker

--- nodes/slot/view.js
+++ nodes/slot/view.js
@@ -2,20 +2,32 @@ var h = require('lib/h')
 var Collection = require('lib/widgets/collection')
 var Spawner = require('lib/widgets/spawner')
 var Range = require('lib/params/range')
+var Select = require('lib/params/select')
+var QueryParam = require('lib/query-param')
+var ToggleChooser = require('lib/params/toggle-chooser')
 
 module.exports = function renderSlot (node) {
   return h('AudioSlot', [
 
     // NOTE: this check could be error prone - consider revising?
-    checkIsTrigger(node) ? h('section', [
-      h('h1', 'Sources'),
-      Collection(node.sources),
-      Spawner(node.sources, {
-        nodes: node.context.nodeInfo.groupLookup.sources.concat(
-          node.context.nodeInfo.groupLookup.drumSources
-        )
-      }),
-    ]) : null,
+    checkIsTrigger(node) ? [
+      h('section', [
+        h('div', {style: {'display': 'flex', 'align-items': 'center', 'flex': '1'}}, [
+          h('h1', 'Sources'),
+          ToggleChooser(QueryParam(node, 'chokeGroup'), {
+            title: 'Choke Group',
+            options: [['None', null], 'A', 'B', 'C', 'D']
+          })
+        ]),
+
+        Collection(node.sources),
+        Spawner(node.sources, {
+          nodes: node.context.nodeInfo.groupLookup.sources.concat(
+            node.context.nodeInfo.groupLookup.drumSources
+          )
+        })
+      ])
+    ] : null,
 
     h('section', [
       h('h1', 'Processors'),
@@ -26,15 +38,13 @@ module.exports = function renderSlot (node) {
     ]),
 
     h('section.options', [
-      h('h1', 'Output'),
       Range(node.volume, {
-        title: 'volume',
+        title: 'output volume',
         flex: true,
         defaultValue: 1,
         format: 'dB'
       })
-    ]),
-
+    ])
   ])
 }
 
