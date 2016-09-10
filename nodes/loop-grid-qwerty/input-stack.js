dada00fd39324f8f864d33495eedc807647c1b48
Matt McKegg <matt@wetsand.co.nz>
2016-09-10 18:32:20 +1200

    handle proper releasing of midiPort and inputStack
    get rid of memory leak

--- nodes/loop-grid-qwerty/input-stack.js
+++ nodes/loop-grid-qwerty/input-stack.js
@@ -3,24 +3,22 @@ var Observ = require('@mmckegg/mutant/value')
 
 module.exports = InputStack
 
-function InputStack(){
-
+function InputStack () {
   var keysDown = null
   var stack = []
   var release = null
 
-  function grab(){
+  function grab () {
     var input = this
     var index = stack.indexOf(input)
-    if (!stack.length || index < stack.length - 1){
+    if (!stack.length || index < stack.length - 1) {
+      release && release()
 
-      release&&release()
-
-      if (stack.length){
-        stack[stack.length-1].set([])
+      if (stack.length) {
+        stack[stack.length - 1].set([])
       }
 
-      if (~index){
+      if (~index) {
         stack.splice(index, 1)
       }
       stack.push(input)
@@ -28,64 +26,67 @@ function InputStack(){
     }
   }
 
-  function close(){
+  function close () {
     var input = this
+    var currentTop = stack[stack.length - 1]
+    var index = stack.indexOf(input)
+    if (~index) {
+      stack.splice(index, 1)
+    }
 
-    if (stack.length){
-      var index = stack.indexOf(input)
-      if (index === stack.length-1){
-        release&&release()
-        stack.pop()
-        release = activate(stack[stack.length-1])
-      }
+    var newTop = stack[stack.length - 1]
+
+    if (newTop !== currentTop) {
+      release && release()
+      release = activate(newTop)
     }
 
     input.set([])
   }
 
-  function activate(input) {
+  function activate (input) {
     if (input) {
       var releaseInput = watch(getKeysDown(), input.set)
       input.active.set(true)
 
-      return function() {
+      return function () {
         releaseInput()
         input.active.set(false)
       }
     }
   }
 
-  function next(){
-    if (stack.length > 1){
+  function next () {
+    if (stack.length > 1) {
       var input = stack.pop()
-      release&&release()
+      release && release()
       input.set([])
       input.active.set(false)
 
       stack.unshift(input)
-      release = activate(stack[stack.length-1])
+      release = activate(stack[stack.length - 1])
     }
   }
 
-  function swap(){
-    if (stack.length > 1){
+  function swap () {
+    if (stack.length > 1) {
       var input = stack.pop()
-      release&&release()
+      release && release()
       input.set([])
       input.active.set(false)
       stack.splice(-1, 0, input)
-      release = activate(stack[stack.length-1])
+      release = activate(stack[stack.length - 1])
     }
   }
 
-  function getKeysDown(){
+  function getKeysDown () {
     return keysDown = keysDown || KeysDown()
   }
 
-  function handleKey(e) {
+  function handleKey (e) {
     var el = document.activeElement
     if (stack.length) {
-      if (!el || (!shouldIgnore(el) && el.contentEditable !== 'true')){
+      if (!el || (!shouldIgnore(el) && el.contentEditable !== 'true')) {
         if ((e.code === 'Tab' || e.code === 'Enter') && e.type === 'keydown') { // tab or enter
           e.preventDefault()
           next()
@@ -100,7 +101,7 @@ function InputStack(){
   document.addEventListener('keydown', handleKey, false)
   document.addEventListener('keyup', handleKey, false)
 
-  return function(){
+  return function () {
     var input = Observ([])
     input.active = Observ()
     input.grab = grab.bind(input)
@@ -108,32 +109,29 @@ function InputStack(){
     input.grab()
     return input
   }
-
 }
 
-
-function shouldIgnore(el){
+function shouldIgnore (el) {
   return (el.nodeName === 'INPUT' && el.type !== 'range' && el.type !== 'checkbox') ||
           el.nodeName === 'TEXTAREA'
 }
 
 function KeysDown () {
-
   var obs = Observ([])
 
   function handleEvent (e) {
     var el = document.activeElement
-    if (!el || (!shouldIgnore(el) && el.contentEditable !== 'true')){
+    if (!el || (!shouldIgnore(el) && el.contentEditable !== 'true')) {
       var index = obs().indexOf(e.code)
-      if (e.type === 'keydown'){
-        if (!~index){
+      if (e.type === 'keydown') {
+        if (!~index) {
           var val = obs().concat()
           val.push(e.code)
           obs.set(val)
           e.preventDefault()
         }
-      } else if (e.type === 'keyup'){
-        if (~index){
+      } else if (e.type === 'keyup') {
+        if (~index) {
           var val = obs().concat()
           val.splice(index, 1)
           obs.set(val)
@@ -145,7 +143,7 @@ function KeysDown () {
   document.addEventListener('keydown', handleEvent, false)
   document.addEventListener('keyup', handleEvent, false)
 
-  obs.destroy = function(){
+  obs.destroy = function () {
     document.removeEventListener('keydown', handleEvent, false)
     document.removeEventListener('keyup', handleEvent, false)
   }
