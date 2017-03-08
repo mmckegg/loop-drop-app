2cbc2093ad72d5e41e972244ce831569e3cbcdb6
Matt McKegg <matt@wetsand.co.nz>
2017-03-08 22:36:33 +1300

    bump mutant and use electron instead of electron-prebuilt

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 var spawn = require('child_process').spawn
-var electron = require('electron-prebuilt')
+var electron = require('electron')
 var join = require('path').join
 
-var app = spawn(electron, ['main.js'], { 
+var app = spawn(electron, ['main.js'], {
   stdio: 'inherit',
-  cwd: join(__dirname, '..') 
+  cwd: join(__dirname, '..')
 }).on('exit', function(i, m) {
   process.exit()
 })
