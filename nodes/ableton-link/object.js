5eb122f85566a9917e886d0f116fb10b08d52178
Matt McKegg <matt@wetsand.co.nz>
2017-11-19 19:07:34 +1300

    ableton-link: fix resume/catchup

--- nodes/ableton-link/object.js
+++ nodes/ableton-link/object.js
@@ -16,6 +16,7 @@ function LinkSync (context) {
 
   var cycleDuration = 60
   var updating = false
+  var beatOffset = Math.round(context.scheduler.getPositionAt(context.audio.currentTime) / 4) * 4
 
   window.offset = 0
 
@@ -23,10 +24,12 @@ function LinkSync (context) {
 
   link.startUpdate(cycleDuration, (beat, phase, bpm) => {
     updating = true
-    context.tempo.set(bpm)
+    if (context.tempo() !== bpm) {
+      context.tempo.set(bpm)
+    }
     var offset = obs.syncOffset() * context.tempo() / 60000
     var currentPosition = context.scheduler.getPositionAt(context.audio.currentTime - 0.02) + offset
-    var difference = (currentPosition + window.offset) - beat
+    var difference = (currentPosition + window.offset) - (beat + beatOffset)
     var multiplier = 1 - difference
     context.scheduler.setSpeed(multiplier)
     updating = false
@@ -43,6 +46,7 @@ function LinkSync (context) {
   obs.context = context
 
   obs.destroy = function () {
+    context.scheduler.setSpeed(1)
     link.stopUpdate()
     link.disable()
     while (releases.length) {
