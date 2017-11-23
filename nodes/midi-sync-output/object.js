2aa77f75371448b0058299542e394cebea3d2040
Matt McKegg <matt@wetsand.co.nz>
2017-11-24 12:32:44 +1300

    midi-sync-output: correct for drift between system and audio device clocks, add destroy

--- nodes/midi-sync-output/object.js
+++ nodes/midi-sync-output/object.js
@@ -16,15 +16,34 @@ function MidiSync (context) {
     length: 1 / 24
   }
 
-  context.scheduler.onSchedule(schedule => {
-    getEvents(clockLoop, schedule.from, schedule.to, 1).forEach(function (event) {
-      if (event[1] && midiPort.stream) {
-        var delta = (event[0] - schedule.from) * schedule.beatDuration
-        var at = schedule.time + delta
-        midiPort.stream.write([248], getMidiTime(at) + 40)
-      }
+  var clockDriftChecker = context.audio.createScriptProcessor(1024 * 8, 0, 1)
+  var lastDifference = 0
+
+  clockDriftChecker.onaudioprocess = function (e) {
+    var currentOffset = Math.round(window.performance.now() - context.audio.currentTime * 1000)
+    var difference = currentOffset - offset
+
+    // remove jitter
+    if (lastDifference !== -difference && currentOffset !== offset) {
+      lastDifference = currentOffset - offset
+      offset = Math.round(currentOffset)
+      console.log(offset)
+    }
+  }
+
+  clockDriftChecker.connect(context.audio.destination)
+
+  var releases = [
+    context.scheduler.onSchedule(schedule => {
+      getEvents(clockLoop, schedule.from, schedule.to, 1).forEach(function (event) {
+        if (event[1] && midiPort.stream) {
+          var delta = (event[0] - schedule.from) * schedule.beatDuration
+          var at = schedule.time + delta
+          midiPort.stream.write([248], getMidiTime(at, offset) + 40)
+        }
+      })
     })
-  })
+  ]
 
   obs.context = context
 
@@ -46,9 +65,18 @@ function MidiSync (context) {
     midiPort.stream.write([0xFC], getMidiTime(at)) // midi start
   }
 
+  obs.destroy = function () {
+    while (releases.length) {
+      releases.pop()()
+    }
+    clockDriftChecker.disconnect()
+    clockDriftChecker.onaudioprocess = null
+    midiPort.destroy()
+  }
+
   return obs
 
-  function getMidiTime (at) {
+  function getMidiTime (at, offset) {
     return at ? (at * 1000) + offset : window.performance.now()
   }
 }
